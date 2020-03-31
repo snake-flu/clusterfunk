0f55bd77e84d65e8da78de6fe18d03d75411a421
jtmccr1 <mccronejt@gmail.com>
2020-03-31 09:50:41 +0100

    adds periods to phyloytpe and cleans up recursion a bit

--- scripts/phylotype.py
+++ scripts/phylotype.py
@@ -7,11 +7,11 @@ def parse_args():
 
     parser.add_argument("input", metavar='input', type=str,
                         help='The input file currently must be a nexus')
-    parser.add_argument("output", metavar='input', type=str,
+    parser.add_argument("output", metavar='output', type=str,
                         help='The output file currently must be a nexus')
     parser.add_argument('--threshold', dest='threshold', action='store',
-                        default=0.00003,
-                        help='branch threshold used to distinguish new phylotype (default: 0.00003')
+                        default=5E-6, type=float,
+                        help='branch threshold used to distinguish new phylotype (default: 5E-6')
     return parser.parse_args()
 
 
@@ -19,27 +19,24 @@ def read_tree(path):
     return dendropy.Tree.get(path=path, schema="nexus")
 
 
-def phylotype(node, parent_phylotype, threshold, increment):
-    current_phylotype = parent_phylotype
+def phylotype_nodes(node, phylotype, threshold):
+    node.phylotype = "\"" + phylotype + "\""
+    node.annotations.add_bound_attribute("phylotype")
 
-    if node.edge.length is not None:
-        if node.taxon is None:
-            if node.edge.length > threshold:
-                current_phylotype = parent_phylotype + str(increment)
-                increment += 1
-
-    node.phylotype = "\"" + current_phylotype + "\""
-    node.annotations.add_bound_attribute("phylotype", datatype_hint="xsd:string")
     i = 0
     for child in node.child_node_iter():
-        i = phylotype(child, current_phylotype, threshold, i)
-    return increment
+        suffix = ""
+        if child.taxon is None:
+            if child.edge.length > threshold:
+                suffix = "." + str(i)
+                i += 1
+        phylotype_nodes(child, phylotype + suffix, threshold)
 
 
 if __name__ == '__main__':
     args = parse_args()
     tree = dendropy.Tree.get(path=args.input, schema="nexus")
-    phylotype(tree.seed_node, "0", args.threshold, 0)
+    phylotype_nodes(tree.seed_node, "0", args.threshold)
     tree.write(path=args.output, schema="nexus")
 
     print("done")
