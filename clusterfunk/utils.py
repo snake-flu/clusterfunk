2e0e555a92c76788297ee160bc5bc08149672d88
jtmccr1 <mccronejt@gmail.com>
2020-04-28 16:15:53 +0100

    fixes edge collapse

--- clusterfunk/utils.py
+++ clusterfunk/utils.py
@@ -20,15 +20,16 @@ def write_tree(tree, file, format):
     tree.write(path=file, schema=format)
 
 
-def collapse_nodes(tree, predicate):
-    for node in tree.preorder_node_iter(predicate):
-        if not node.is_leaf():
-            node.edge.collapse(adjust_collapsed_head_children_edge_lengths=True)
+def collapse_nodes(tree, threshold):
+    for node in tree.postorder_node_iter(lambda n: not n.is_leaf()):
+        if not node == tree.seed_node:
+            if node.edge_length <= threshold:
+                node.edge.collapse(adjust_collapsed_head_children_edge_lengths=True)
 
 
 def prepare_tree(options, input_file_name=None):
     input_path = input_file_name if input_file_name is not None else options.input
     tree = parse_tree(input_path, options.format)
     if options.collapse:
-        collapse_nodes(tree, lambda node: node.edge.length <= options.collapse)
+        collapse_nodes(tree, options.collapse)
     return tree
