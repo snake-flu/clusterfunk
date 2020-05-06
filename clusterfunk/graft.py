5fa87d5d2cec3c269139b86a293fb4dab28432d6
jtmccr1 <mccronejt@gmail.com>
2020-05-07 00:05:55 +0100

    updates graft to update leaf bitmap

--- clusterfunk/graft.py
+++ clusterfunk/graft.py
@@ -14,16 +14,25 @@ class RootStock:
 
         shared_taxa = [tip for tip in tips if tip.label in incoming_tree_tips_labels]
 
-        if len(shared_taxa)==0:
+        if len(shared_taxa) == 0:
             raise KeyError("Didn't find any shared tips between the rootstock and scion")
 
         expectant_parent = self.tree.mrca(taxa=shared_taxa)
-        added_node = expectant_parent.add_child(incoming_tree.seed_node)
-        added_node.edge.collapse(adjust_collapsed_head_children_edge_lengths=True)
+
+        if expectant_parent.is_leaf():
+            length = expectant_parent.edge_length
+            expectant_parent = expectant_parent.parent_node
+            added_node = expectant_parent.add_child(incoming_tree.seed_node)
+            added_node.edge_length = length
+        else:
+            added_node = expectant_parent.add_child(incoming_tree.seed_node)
+            added_node.edge.collapse(adjust_collapsed_head_children_edge_lengths=True)
 
         self.tree.prune_taxa(shared_taxa)
         self.tree.purge_taxon_namespace()
+        self.tree.update_bipartitions()
 
     def remove_left_over_tips(self):
         self.tree.prune_taxa([taxon for taxon in self.tree.taxon_namespace if taxon not in self.added_taxa])
         self.tree.purge_taxon_namespace()
+        self.tree.update_bipartitions()
--- tests/graft.py
+++ tests/graft.py
@@ -9,35 +9,26 @@ data_dir = os.path.join(this_dir, "tests", 'data', 'graft')
 
 
 class test_grafting_big_trees(unittest.TestCase):
-    def test_all_are_added(self):
-        base_tree = dendropy.Tree.get(path="%s/representative_sequences.aln.tree" % data_dir, schema="nexus")
-        A = dendropy.Tree.get(path="%s/COG_GISAID_2020-04-11_lineage_A.tree" % data_dir, schema="newick")
-        for node in A.postorder_node_iter():
-            node.base_lineage = "A"
-            node.annotations.add_bound_attribute("base_lineage")
+    def setUp(self):
+        tree = dendropy.Tree.get(data="((A:1,B:1):3,C:4);", schema="newick", rooting="default-rooted")
+        self.rootStock = RootStock(tree)
 
-        B = dendropy.Tree.get(path="%s/COG_GISAID_2020-04-11_lineage_B.tree" % data_dir, schema="newick")
-        for node in B.postorder_node_iter():
-            node.base_lineage = "B"
-            node.annotations.add_bound_attribute("base_lineage")
-        B1 = dendropy.Tree.get(path="%s/COG_GISAID_2020-04-11_lineage_B.1.tree" % data_dir, schema="nexus")
-        for node in B1.postorder_node_iter():
-            node.base_lineage = "B1"
-            node.annotations.add_bound_attribute("base_lineage")
+    def test_tip_graft(self):
+        incoming = dendropy.Tree.get(data="((aC:1,aB:1):3,A:4);", schema="newick", rooting="default-rooted")
+        self.rootStock.graft(incoming)
 
-        expected_tips = len(A.leaf_nodes()) + len(B.leaf_nodes()) + len(B1.leaf_nodes())
-        print(expected_tips)
-        guide_tree = RootStock(base_tree)
+        print(self.rootStock.tree.as_ascii_plot())
+        self.assertTrue(True)
 
-        guide_tree.graft(A)
-        guide_tree.graft(B)
-        guide_tree.graft(B1)
-        guide_tree.remove_left_over_tips()
+    def test_tip_graft(self):
+        incoming = dendropy.Tree.get(data="((aC:1,aB:1):3,A:4);", schema="newick", rooting="default-rooted")
+        incoming2 = dendropy.Tree.get(data="((a2C:1,a2B:1):3,A:4);", schema="newick", rooting="default-rooted")
 
-        # guide_tree.tree.write(path="%s/combined_tree.tree" % data_dir, schema="nexus")
-        # print(len(guide_tree.tree.leaf_nodes()))
+        self.rootStock.graft(incoming)
+        self.rootStock.graft(incoming2)
 
-        self.assertEqual(len(guide_tree.tree.leaf_nodes()), expected_tips)
+        print(self.rootStock.tree.as_ascii_plot())
+        self.assertTrue(True)
 
 
 if __name__ == '__main__':
